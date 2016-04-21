c86b74d75a8ccf02e7b347058ad1ffc05e608708
snmslavk <avsenev@gmail.com>
2016-04-21 12:09:09 +0300

    initial

---
+++ Streamable.dotNET/Interfaces/IStreamableAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Streamable.dotNET.Models;
+
+namespace Streamable.dotNET.Interfaces
+{
+    public interface IStreamableAPI
+    {
+        UploadVideoResponse Upload();
+        UploadVideoResponse Import();
+        void Retrive();
+        void RetrieveUser();
+        void RetrieveAuthUser();
+        void GEToEmbed();
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string url = "https://api.streamable.com/upload";
+                string filePath = "c:\\temp\\zyra.mp4";
+                string userName = "snmslavk";
+                string password = "33looking";
+
+                string credentials = Convert.ToBase64String(
+                   Encoding.ASCII.GetBytes(userName + ":" + password));
+
+                client.Headers[HttpRequestHeader.Authorization] = string.Format(
+                    "Basic {0}", credentials);
+
+                var zz = Encoding.UTF8.GetString(
+                    client.UploadFile(url, filePath)
+                    );
+            }
+        }
+    }
+}
